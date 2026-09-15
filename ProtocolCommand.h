62cedaa34a5037c66744200296ba27bd1a059351
shabunicholas <2889565370@qq.com>
2026-09-15 21:49:30 +0800

    添加收发定义

--- ProtocolCommand.h
+++ ProtocolCommand.h
@@ -1,9 +1,22 @@
 #ifndef PROTOCOLCOMMAND_H
 #define PROTOCOLCOMMAND_H
 #include<cstdint>
+#include<QByteArray>
+#define MAX_PACK 9
+//static constexpr int MAX_PACK=9;
 enum class COMMAND : uint8_t{
     WEN_DU_REQ=0x01,
     WEN_DU_RSP=0x02
 };
+
+class ProtocolCodec{
+public:
+    static QByteArray pack(uint8_t cmd,uint8_t param ,const QByteArray &data=QByteArray());
+    static bool parse(const QByteArray &frame,uint8_t &cmd,uint8_t &param,QByteArray &data);
+private:
+    static constexpr uint8_t frame_header=0xEF;
+    static constexpr uint8_t frame_tail=0xFE;
+};
+
 
 #endif // PROTOCOLCOMMAND_H
