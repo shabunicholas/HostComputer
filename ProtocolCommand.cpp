203230d0bff40e9e27ad56a2ec14f0393116256e
shabunicholas <2889565370@qq.com>
2026-09-17 19:36:01 +0800

    添加封装与解析

---
+++ ProtocolCommand.cpp
@@ -0,0 +1,43 @@
+#include"ProtocolCommand.h"
+
+#include<QDebug>
+QByteArray ProtocolCodec::pack(uint8_t cmd, uint8_t param, const QByteArray &data)
+{
+    QByteArray array;
+    array.append(frame_header);
+    array.append(cmd);
+    array.append(param);
+    array.append(data);
+    array.append(frame_tail);
+
+    return array;
+}
+
+bool ProtocolCodec::parse(const QByteArray &frame, uint8_t &cmd, uint8_t &param, QByteArray &data)
+{
+    if(frame.size()<9)return false;
+    uint8_t head=frame.at(0);
+    uint8_t tail=frame.at(8);
+
+    if(head!=frame_header || tail!=frame_tail){
+        return false;
+    }
+
+    //这里使用奇偶效验，后面接入moubus协议crc 效验
+    //9个字节
+    //[包头][命令][参数][data][data][data][data][效验位][包尾]
+    int count=0;
+    for(int i=0;i<7;i++){
+
+    }
+
+    //小端
+    cmd=frame.at(1);
+    param=frame.at(2);
+    data=frame.mid(3,4);
+    int data_int= (int)(frame.at(3)) | (frame.at(4)<<8) |
+            (frame.at(5)<<16) | (frame.at(6)<<24);
+    qDebug()<<"数据(int):"<<data_int;
+
+    return true;
+}
